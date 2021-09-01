899e523e62e4331a7fb9bd7caf3f47903bab3a67
darya-startsava <daryastartsava@gmail.com>
2021-09-01 14:05:01 +0200

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let arr = [];
+        let mid, min, max;
+        this.mid = mid;
+        this.arr = arr;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        for (let i = min; i <= max; i++) {
+            this.arr.push(i);
+        };
+        return this.arr;
     }
 
     guess() {
 
+        this.mid = Math.round((this.max - this.min) / 2) + this.min;
+        return this.mid;
+
     }
 
     lower() {
+        this.max = this.mid;
+        return this.max;
 
     }
 
     greater() {
-
+        this.min = this.mid;
+        return this.min;
     }
 }
 
